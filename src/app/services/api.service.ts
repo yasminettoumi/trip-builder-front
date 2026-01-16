6b5c152bb667d4f3e080ea9afd9fb0eab22f6871
Yasmine Ettoumi <yasminettoumi@gmail.com>
2026-01-16 14:16:44 -0500

    update backend api with remote address

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ApiService {
-  private baseUrl = 'http://127.0.0.1:8000/api';
+  private baseUrl = 'https://trip-builder-1.onrender.com/api';
 
   constructor(private http: HttpClient) {}
 
